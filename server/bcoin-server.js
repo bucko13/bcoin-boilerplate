8b17449b28f782aafaa8e8107e3c6b3a47d458ea
Buck Perley <buck@eggplantdigital.cn>
2017-02-23 22:20:34 -0800

    cleaned up node to use proper databases, fixed auth of router

--- server/bcoin-server.js
+++ server/bcoin-server.js
@@ -1,36 +1,46 @@
 const bcoin = require('bcoin');
 const API_KEY = require('../config.js').BCOIN_API_KEY;
 
-const SPVNode = bcoin.spvnode;
+const FullNode = bcoin.fullnode;
+const port = +process.argv[2] || 8080;
 
 const options = bcoin.config({
   network: 'testnet',
   walletauth: true,
+  useWorkers: true,
+  coinCache: 30000000,
+  query: true,
   passphrase: API_KEY,
-  apiKey: API_KEY,
-  // logLevel: 'info',
+  apiKey: '12345',
+  pruned: true,
+  db: 'leveldb',
+  logLevel: 'info',
 });
 bcoin.set(options);
 
-const node = new SPVNode(options);
-const walletdb = bcoin.walletdb({
-  location: process.env.HOME.concat('/walletdb'),
-  db: 'leveldb',
-  name: 'my wallet db',
-});
-
-node.walletdb = walletdb;
+const node = new FullNode(options);
 
 node.on('error', () => {
   ; // eslint-disable-line no-extra-semi
 });
-node.open()
-.then(() => {
-  node.connect().then(() => {
-    node.startSync();
-  });
-});
-const server = new bcoin.http.Server({ node });
-const port = +process.argv[2] || 8080;
 
-server.listen(port, '127.0.0.1');
+node.pool.on('open', () => {
+  console.log('pool opened');
+});
+
+node.mempool.on('tx', (tx) => {
+  console.log('******** Saw tx: ', tx);
+});
+
+
+node.chain.on('block', () => {
+  console.log('new block found. Chain State tx: ', node.chain.tip);
+});
+
+node.open()
+.then(() => node.connect())
+.then(() => {
+  node.startSync();
+});
+
+node.http.listen(port, '127.0.0.1');
--- server/nodeRouter.js
+++ server/nodeRouter.js
@@ -1,5 +1,8 @@
+'use strict';
+
 const express = require('express');
 const request = require('request');
+const auth = require('basic-auth');
 
 const nodeRouter = express.Router({ mergeParams: true });
 const bcoinPort = 8080;
@@ -8,12 +11,22 @@ const baseRequest = request.defaults({
 });
 
 nodeRouter.use((req, res) => {
+  let authorization;
+
+  if (auth(req)) {
+    authorization = {
+      user: auth(req).name,
+      pass: auth(req).pass,
+      sendImmediately: false,
+    };
+  }
   const options = {
     method: req.method,
     uri: req.path,
     body: req.body,
     json: true,
     qs: req.query,
+    auth: authorization,
   };
   baseRequest(options, (err, resp, body) => {
     if (err) {
