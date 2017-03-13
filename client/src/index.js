969e061ce83b4b01713c89f483181fb42ce39fea
Buck Perley <buck@eggplantdigital.cn>
2017-03-12 22:26:51 -0700

    html forms are included via a require to make index.html more customizable

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,11 @@
 import $ from 'jquery';
 import submitAction from './submitAction';
 
+const indexTemplate = require('../templates/template.html');
+
 $(() => {
   const formActionButton = $('form button');
-
+  $('.app').html(indexTemplate);
   $.get('/node/fee', (data) => {
     const value = data.rate;
     $('input[name="fee"]').val(value);
