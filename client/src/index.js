9c9dd6fa325e9b27147303b69b0c7843a40b48a2
Buck Perley <buck@eggplantdigital.cn>
2017-03-12 21:16:13 -0700

    clean up index.js by splitting out submitAction into separate module

--- client/src/index.js
+++ client/src/index.js
@@ -1,47 +1,13 @@
 import $ from 'jquery';
-import { reqProps, checkInputs } from './utils';
+import submitAction from './submitAction';
 
 $(() => {
   const formActionButton = $('form button');
-  const messageContainer = $('.server-messages');
-  const apiKey = $('input[name="apiKey"]');
-  const nodeEndpoint = '/node';
 
   $.get('/node/fee', (data) => {
     const value = data.rate;
     $('input[name="fee"]').val(value);
   });
 
-  formActionButton.on('click', function submitClick(event) {
-    event.preventDefault();
-
-    const action = $(this).attr('data-action');
-    const form = $(this).parent();
-
-    if (!checkInputs(action, form)) return;
-
-    const reqPropsMap = reqProps(form)[action];
-    const type = reqPropsMap.type;
-    const url = nodeEndpoint.concat(reqPropsMap.url);
-    const data = reqPropsMap.data ? JSON.stringify(reqPropsMap.data) : '';
-
-    $.ajax({
-      type,
-      url,
-      data,
-      processData: false,
-      beforeSend: (xhr) => {
-        xhr.setRequestHeader('Authorization', 'Basic ' + btoa('' + ':' + apiKey.val()));
-      },
-      contentType: 'application/json',
-    }).done((response) => {
-      const prettyResponse = JSON.stringify(response, null,'\t');
-      const message =
-        '<h4>Server Response:</h4>'
-        .concat('<pre class="server-message" style="width:75%;">')
-        .concat(prettyResponse)
-        .concat('</pre>');
-      messageContainer.html(message);
-    });
-  });
+  formActionButton.on('click', submitAction);
 });
