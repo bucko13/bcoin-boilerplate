ae4435d41fc02fee8b2df3fa52d94cced32ebe88
Buck Perley <buck@eggplantdigital.cn>
2017-03-19 20:05:40 -0700

    added support for other input types in react forms

--- client/react-src/components/FormInput.jsx
+++ client/react-src/components/FormInput.jsx
@@ -4,17 +4,37 @@ const FormInput = ({
   label,
   name,
   type,
-}) => (
-  <div className="form-group">
-    <label htmlFor={name}>{label}</label>
-    <input type={type} name={name} className={`form-control ${name}`} />
-  </div>
-);
+  min,
+  step,
+}) => {
+  let input;
+  if (type === 'textarea') {
+    input = <textarea name={name} className={`form-control ${name}`} />;
+  } else {
+    input =
+    (<input
+      type={type}
+      name={name}
+      className={`form-control ${name}`}
+      min={min}
+      step={step}
+    />);
+  }
+
+  return (
+    <div className="form-group">
+      <label htmlFor={name}>{label}</label>
+      {input}
+    </div>
+  );
+};
 
 FormInput.propTypes = {
   label: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
+  min: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  step: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 };
 
 export default FormInput;
--- client/react-src/containers/FormContainer.jsx
+++ client/react-src/containers/FormContainer.jsx
@@ -1,40 +1,88 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { ajax } from 'jquery';
 import FormInput from '../components/FormInput';
-import submitAction from '../../src/submitAction';
+import { reqProps, checkInputs } from '../../utils/utils';
 
-const FormContainer = ({
-  title,
-  actionName,
-  formClass = '',
-  formInputs,
-}) => (
-  <div className="panel panel-primary">
-    <div className="panel-heading">
-      <h3>{title}</h3>
-    </div>
-    <div className="panel-body">
-      <form className={formClass}>
-        {
-          formInputs.map(({ label, name, type }) => (
-            <FormInput
-              label={label}
-              name={name}
-              type={type}
-              key={name}
-            />
-          ))
+class FormContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.nodeEndpoint = '/node';
+    this.state = {
+      serverMessage: null,
+    };
+  }
+
+  submitForm(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    const { actionName } = this.props;
+    if (!checkInputs(actionName, this.form)) return;
+
+    // eslint-disable-next-line prefer-const
+    let { type, url, data } = reqProps(this.form)[actionName];
+    url = this.nodeEndpoint.concat(url);
+    data = data ? JSON.stringify(data) : '';
+
+    ajax({
+      type,
+      url,
+      data,
+      processData: false,
+      contentType: 'application/json',
+      // TODO: clean this up
+      // removed beforeSend authorization function that's in default build
+      // auth is being done by default on the nodeRouter.
+    })
+    .done(response => this.setState({ serverMessage: response }));
+  }
+
+  render() {
+    const {
+      title,
+      actionName,
+      formClass = '',
+      formInputs,
+    } = this.props;
+
+    const composeInput = ({ label, name, type, min, step }) => (
+      <FormInput
+        label={label}
+        name={name}
+        type={type}
+        min={min}
+        step={step}
+        key={name}
+      />
+    );
+
+    return (
+      <div className="panel panel-primary" ref={(form) => { this.form = form; }}>
+        <div className="panel-heading">
+          <h3>{title}</h3>
+        </div>
+        <div className="panel-body">
+          <form className={formClass}>
+            {formInputs.map(composeInput)}
+            <button
+              className="btn btn-success pull-right"
+              data-action={actionName}
+              onClick={event => this.submitForm(event)}
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+        {!!this.state.serverMessage &&
+          <div className="well server-message">
+            <h4>Response from Server:</h4>
+            <pre>{JSON.stringify(this.state.serverMessage, null, '\t')}</pre>
+          </div>
         }
-        <button
-          className="btn btn-success pull-right"
-          data-action={actionName}
-          onClick={event => submitAction(event)}
-        >
-          Submit
-        </button>
-      </form>
-    </div>
-  </div>
-);
+      </div>
+    );
+  }
+}
 
 FormContainer.propTypes = {
   title: PropTypes.string.isRequired,
