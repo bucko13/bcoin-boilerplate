b001eee775535d180ae0aff14a963365695fb298
josh <belljh@gmail.com>
2017-03-25 15:13:49 -0700

    WIP fail on using require

--- client/magnetLinkUtils.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by josh on 3/25/17.
- */
-
-const myLink = document.getElementById('submitMagnetLinkButton');
-debugger;
-myLink.onclick = function () {
-  alert('clicked magnet button');
-};
